de7e75a0777ba1b176801f360aa9ed31b9092ad0
Artem <tema.rastaclaus@gmail.com>
2017-05-31 12:50:24 +0300

    hanoy completed

--- hanoy.py
+++ hanoy.py
@@ -2,4 +2,13 @@
 # -*- encoding=utf-8 -*-
 
 
+def hanoy(q, fr, to, bf):
+    if q == 0:
+        return
+    hanoy(q-1, fr, bf, to)
+    print(fr, '-',  to)
+    hanoy(q-1, bf, to, fr)
 
+if __name__ == '__main__':
+    q = int(input())
+    hanoy(q, 1, 3, 2)
