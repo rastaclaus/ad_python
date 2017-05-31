6effad01895aa8edd3871a1bf15a3439db139598
tema.rastaclaus <tema.rastaclaus@gmail.com>
2017-05-31 22:52:05 +0300

    ..and again...

--- basic_rle_encode.py
+++ basic_rle_encode.py
@@ -4,8 +4,16 @@
 import re
 
 if __name__ == '__main__':
+    instr = 'a2a11b5c2CaB'
     # instr = input()
-    nums = re.compile('\d+')
-    instr = 'a2ab45c2CaB'
-    for m in nums.finditer(instr):
-        print(m.start(), m.group())
+    nums = re.compile('(\d+)([A-Za-z]){1}')
+    inlist = list(instr)
+    coded = nums.finditer(instr)
+    bias = 0
+    for res in coded:
+        len0 = len(inlist)
+        start = res.start() + bias
+        end = res.end() + bias
+        inlist[start:end] = int(res.groups()[0]) * res.groups()[1]
+        bias += len(inlist) - len0
+    print(''.join(inlist))
