ea460da469a4643e213c6273c617c52d145eb764
tema.rastaclaus <tema.rastaclaus@gmail.com>
2017-06-07 08:35:42 +0300

    continut spirale

--- spirale.py
+++ spirale.py
@@ -3,7 +3,7 @@
 
 
 def directions():
-    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     while True:
         for d in dirs:
             yield(d)
@@ -22,15 +22,22 @@ def spirale_index(n):
         if count == l:
             count = 0
             dir = next(dirs)
-            if dir == (0, -1):
+            if dir == (-1, 0):
                 l -= 1
+
+
+def get_sqmattr(n):
+    return [[None for _ in range(n)] for _ in range(n)]
 
 
 if __name__ == '__main__':
     from pprint import pprint
     n = int(input())
-    matr = [[None] * n]*n
+    matr = get_sqmattr(n)
+    im = spirale_index(n)
     pprint(matr)
-    for i in range(n):
-        matr[1] = 1
+    for i, row, col in im:
+        print(i, row, col)
+        matr[row][col] = i
         pprint(matr)
+    pprint(matr)
